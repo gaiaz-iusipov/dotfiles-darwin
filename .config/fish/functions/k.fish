d4d6df6e6e136574dfa8a201dd56a0afb12f447d
Gaiaz Iusipov <g.iusipov@gmail.com>
2024-06-08 22:40:06 +0400

    Remove fish shell config

--- .config/fish/functions/k.fish
+++
@@ -1,4 +0,0 @@
-function k --wraps=kubectl --description 'alias k=kubectl'
-  kubectl $argv
-        
-end
--- .config/fish/functions/kg.fish
+++
@@ -1,4 +0,0 @@
-function kg --wraps='kubectl get' --description 'alias kg=kubectl get'
-  kubectl get $argv
-        
-end
